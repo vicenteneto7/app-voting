f860efabc572d24435b9186af4ac06b98a4f5efb
Vicente <pvicenteneto7@gmail.com>
2024-09-11 11:24:36 -0300

    feat: estruturação das rotas

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
-import { useState } from "react";
-import { MyRoutes } from "./src/routes/routes";
-import LoginScreen from "./src/Screens/Login";
-import { NavigationContainer } from "@react-navigation/native";
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ThemeProvider } from "styled-components";
-import theme from "./src/global/styles/theme";
-///<Stack.Screen name="Login" component={LoginScreen} />
 
+import theme from "./src/global/styles/theme";
+
+import { Routes } from "./src/routes";
+import { EleitorProvider } from "./src/hooks/auth";
+
+//<Stack.Screen name="Login" component={LoginScreen} />
 
 export default function App() {
-
   const Stack = createNativeStackNavigator();
 
   return (
     <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Tabs" component={MyRoutes} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <EleitorProvider>
+        <Routes />
+      </EleitorProvider>
     </ThemeProvider>
   );
 }
